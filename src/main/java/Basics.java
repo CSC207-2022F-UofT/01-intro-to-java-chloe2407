54a4765006b3da016f8d73686adfbeb1ad851ad8
Chloe Lam <president@uclit.ca>
2022-09-19 22:12:24 -0400

    submit assignment

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+
+        for(int i = 10; i >= 0; i-- ) {
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -140,10 +146,14 @@ public class Basics {
          *                separated by spaces, so no loops are required, though
          *                you may use them if you wish.
          */
+
+//      Create a String named 'ret'
         StringBuilder ret = new StringBuilder();
-
-        // Fill in the rest of the body here
-
+//      Put words into a list by splitting by space
+//      get the first letter of each word in words (length is predetermined to length 7
+        for(int i = 0; i < 7; i++) {
+            ret.append(to_split.split(" ")[i].charAt(0));
+        }
         return ret.toString();
     }
 
@@ -170,7 +180,9 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        for(int i = 0; i < arr.length; i++) {
+            current_sum += arr[i];
+        }
         return current_sum;
     }
 
